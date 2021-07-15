4527916279429e82267f61490a6bea6db61a54d2
MikeHengXY <2452108058@qq.com>
2021-07-15 09:51:17 -0700

    test get shmi

--- example/shm/main.go
+++ example/shm/main.go
@@ -7,23 +7,28 @@ import (
 	"log"
 )
 
-type LogItem struct {
-	ProtocolName string
-	Fields       []string
-	Data         []interface{}
-}
+//type LogItem struct {
+//	ProtocolName string
+//	Fields       []string
+//	Data         []interface{}
+//}
 //go build -o readshm example/shm/main.go
+//func testConstructor() interface{} {
+//	return &LogItem{}
+//}
 func testConstructor() interface{} {
-	return &LogItem{}
+	return &shmdata.TagTLV{}
 }
 
+//todo run this please run del-shm.sh
 func main() {
 
-	shmi, err := shmdata.GetShareMemoryInfo(999999)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(shmi)
+	//shmi, err := shmdata.GetShareMemoryInfo(999999)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println(shmi)
 
 	mem, err := shm.NewSystemVMem(6, 10000)
 	if err != nil {
@@ -36,17 +41,17 @@ func main() {
 		return
 	}
 
-	for i := 0; i < 10; i++ {
-		item := &LogItem{
-			ProtocolName: "1",
-			Fields:       []string{fmt.Sprintf("field-%d", i)},
-			Data:         []interface{}{i},
-		}
-		if err := s.Save(item); err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
+	//for i := 0; i < 10; i++ {
+	//	item := &LogItem{
+	//		ProtocolName: "1",
+	//		Fields:       []string{fmt.Sprintf("field-%d", i)},
+	//		Data:         []interface{}{i},
+	//	}
+	//	if err := s.Save(item); err != nil {
+	//		fmt.Println(err)
+	//		return
+	//	}
+	//}
 
 	items, err := s.Get()
 	if err != nil {
