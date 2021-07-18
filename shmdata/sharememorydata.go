e8e8f7b7d2acc422c322dddeb0783c18869b5ba0
MikeHengXY <2452108058@qq.com>
2021-07-18 02:04:36 -0700

    fix struct for shmi type

--- shmdata/sharememorydata.go
+++ shmdata/sharememorydata.go
@@ -3,6 +3,7 @@ package shmdata
 import (
 	"fmt"
 	"github.com/kevinu2/shm"
+	"reflect"
 	"unsafe"
 )
 
@@ -17,13 +18,19 @@ import (
 //} SHMI, *PSHMI;
 
 //todo this will read from  key of 999999  SHM
-
+//typedef struct SHMInfo{
+//unsigned long long max_topic_len;
+//unsigned long long max_content_len;
+//unsigned long long max_shm_size;
+//unsigned long long count;
+//key_t key[200];
+//}SHMI;
 type SHMInfo struct {
-	MaxTopicLen   uint32
-	MaxContentLen uint32
-	MaxSHMSize    uint32
-	Count         uint32
-	Key           [200]uint32
+	MaxTopicLen   uint64
+	MaxContentLen uint64
+	MaxSHMSize    uint64
+	Count         uint64
+	Key           [200]int32
 }
 
 var MTL uint = 64
@@ -56,3 +63,77 @@ func GetShareMemoryInfo(defaultKey int) (SHMInfo, error) {
 	return shmi, err
 }
 
+func SizeStruct(data interface{}) int {
+	return sizeof(reflect.ValueOf(data))
+}
+
+func sizeof(v reflect.Value) int {
+	switch v.Kind() {
+	case reflect.Map:
+		sum := 0
+		keys := v.MapKeys()
+		for i := 0; i < len(keys); i++ {
+			mapkey := keys[i]
+			s := sizeof(mapkey)
+			if s < 0 {
+				return -1
+			}
+			sum += s
+			s = sizeof(v.MapIndex(mapkey))
+			if s < 0 {
+				return -1
+			}
+			sum += s
+		}
+		return sum
+	case reflect.Slice, reflect.Array:
+		sum := 0
+		for i, n := 0, v.Len(); i < n; i++ {
+			s := sizeof(v.Index(i))
+			if s < 0 {
+				return -1
+			}
+			sum += s
+		}
+		return sum
+
+	case reflect.String:
+		sum := 0
+		for i, n := 0, v.Len(); i < n; i++ {
+			s := sizeof(v.Index(i))
+			if s < 0 {
+				return -1
+			}
+			sum += s
+		}
+		return sum
+
+	case reflect.Ptr, reflect.Interface:
+		p := (*[]byte)(unsafe.Pointer(v.Pointer()))
+		if p == nil {
+			return 0
+		}
+		return sizeof(v.Elem())
+	case reflect.Struct:
+		sum := 0
+		for i, n := 0, v.NumField(); i < n; i++ {
+			s := sizeof(v.Field(i))
+			if s < 0 {
+				return -1
+			}
+			sum += s
+		}
+		return sum
+
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128,
+		reflect.Int:
+		return int(v.Type().Size())
+
+	default:
+		fmt.Println("t.Kind() no found:", v.Kind())
+	}
+
+	return -1
+}
