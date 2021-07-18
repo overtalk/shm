b819da1038f2ac42e0ad46ba0a15de72ab28e482
MikeHengXY <2452108058@qq.com>
2021-07-18 05:10:27 -0700

    read shmi and tlv data

--- example/ishm/main.go
+++ example/ishm/main.go
@@ -11,10 +11,10 @@ import (
 	"unsafe"
 )
 
-const MAX_SIZE= 1 << 30
+const MAX_SIZE = 1 << 30
 
-func ReadSHMI()  {
-	sm,err:=ishm.CreateWithKey(12,MAX_SIZE)
+func WriteReadSHMI() {
+	sm, err := ishm.CreateWithKey(12, MAX_SIZE)
 	if err != nil {
 		log.Fatal(err)
 		sm.Destroy()
@@ -22,39 +22,72 @@ func ReadSHMI()  {
 	var buf bytes.Buffer
 	encoder := gob.NewEncoder(&buf) // will write to buf
 
-	shmi:=shmdata.SHMInfo{}
-	lll:=shmdata.SizeStruct(shmi)
-	fmt.Printf("shmisize:%v ,,,sizof:%v\n",lll,unsafe.Sizeof(shmi))
-	shmi.MaxSHMSize=100
-	shmi.MaxContentLen=64
-	shmi.MaxTopicLen=128
-	shmi.Count=4
-	for i,_:=range shmi.Key{
-		shmi.Key[i]=rand.Int31()
+	shmi := shmdata.SHMInfo{}
+	lll := shmdata.SizeStruct(shmi)
+	fmt.Printf("shmisize:%v ,,,sizof:%v\n", lll, unsafe.Sizeof(shmi))
+	shmi.MaxSHMSize = 100
+	shmi.MaxContentLen = 64
+	shmi.MaxTopicLen = 128
+	shmi.Count = 4
+	for i, _ := range shmi.Key {
+		shmi.Key[i] = rand.Int31()
 	}
-	fmt.Printf("shm org:%#v\n",shmi)
+	fmt.Printf("shm org:%#v\n", shmi)
 	encoder.Encode(shmi)
 	sm.Write(buf.Bytes())
-	shmilen:=buf.Len()//unsafe.Sizeof(shmi)
+	shmilen := buf.Len() //unsafe.Sizeof(shmi)
 
-	fmt.Printf("buferlen:%v\n",shmilen)
-	od,err:=sm.ReadChunk(int64(shmilen),0 )
+	fmt.Printf("buferlen:%v\n", shmilen)
+	od, err := sm.ReadChunk(int64(shmilen), 0)
 	if err != nil {
 		log.Fatal(err)
 	}
 	buf.Reset()
 	buf.Write(od)
 	decoder := gob.NewDecoder(&buf) // will read from buf
-	smrd:=shmdata.SHMInfo{}
-	err=decoder.Decode(&smrd)
+	smrd := shmdata.SHMInfo{}
+	err = decoder.Decode(&smrd)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("shm read:%#v\n",smrd)
-	fmt.Printf("sm:%#v\n",sm)
+	fmt.Printf("shm read:%#v\n", smrd)
+	fmt.Printf("sm:%#v\n", sm)
 	sm.Destroy()
 }
 func main() {
+	shmi, err := shmdata.GetShareMemoryInfo(999999)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//	log.Print(shmi)
 
-	ReadSHMI()
+	for i, k := range shmi.Key {
+		if i == int(shmi.Count) {
+			break
+		}
+		fmt.Printf("key:%v\r\n", k)
+
+		sm, err := ishm.CreateWithKey(int64(k), 0)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		log.Print(sm)
+
+		for {
+			tlv := shmdata.TagTLV{}
+			tlv.Topic = make([]byte, shmi.MaxTopicLen)
+			tlv.Value = make([]byte, shmi.MaxContentLen)
+			datalen := shmdata.SizeStruct(tlv)
+			od, err := sm.ReadChunk(int64(datalen), int64(sm.Position()+16))
+
+			if err != nil {
+
+			}
+
+			fmt.Printf("tlvdata:%#v", od)
+		}
+
+		i++
+	}
 }
