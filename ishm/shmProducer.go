ef536758b05c047629624578c88f07b465746fe0
MikeHengXY <2452108058@qq.com>
2021-07-24 06:26:38 -0700

    fist tlvuse for updatecontent

--- ishm/shmProducer.go
+++ ishm/shmProducer.go
@@ -2,7 +2,10 @@ package ishm
 
 import (
 	"log"
+	"reflect"
+	"unsafe"
 )
+
 
 type CreateSHMParam struct {
 	Key int64
@@ -13,34 +16,74 @@ type UpdateContent struct {
 	Topic string
 	Content string
 }
-func UpdateCtx(shmparam CreateSHMParam, updatectx UpdateContent) (index int, err error){
+func StringToByteArr(s string, arr []byte) {
+	src := []rune(s)
+	for i, v := range src {
+		if i >= len(arr) {
+			break
+		}
+		arr[i] = byte(v)
+	}
+}
 
+var sizeOfTagTLVStruct = int(unsafe.Sizeof(TagTLV{}))
+
+func TagTLVStructToBytes(s *TagTLV) []byte {
+	var x reflect.SliceHeader
+	x.Len = sizeOfTagTLVStruct
+	x.Cap = sizeOfTagTLVStruct
+	x.Data = uintptr(unsafe.Pointer(s))
+	return *(*[]byte)(unsafe.Pointer(&x))
+}
+
+func BytesToTagTLVStruct(b []byte) *TagTLV {
+	return (*TagTLV)(unsafe.Pointer(
+		(*reflect.SliceHeader)(unsafe.Pointer(&b)).Data,
+	))
+}
+
+func UpdateCtx(shmparam CreateSHMParam, updatectx UpdateContent) (index int, err error){
+	log.Printf("UpdateCtx:%#v,%#v",shmparam,updatectx)
+	tlv:=TagTLV{}
+	if shmparam.Size < int64(unsafe.Sizeof(tlv)) {
+		shmparam.Size =int64(unsafe.Sizeof(tlv))
+	}
 	sm,err:= CreateWithKey(shmparam.Key,shmparam.Size)
 	if err != nil {
 		log.Fatal(err)
 		return index,err
 	}
-	log.Print(sm)
-	pos ,err:=sm.WriteObj(updatectx)
+	tlv.Tag=1
+	tlv.Len=uint64(len(updatectx.Content))
+	StringToByteArr(updatectx.Topic,tlv.Topic[:])
+	StringToByteArr(updatectx.Content,tlv.Value[:])
+	wd:=TagTLVStructToBytes(&tlv)
+	sm.Write(wd)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print(pos)
-
-	return pos,err
+	return int(sm.Id),err
 }
-func GetCtx(shmparam CreateSHMParam) ( updatectx*UpdateContent, err error){
-
+func GetCtx(shmparam CreateSHMParam)   (*UpdateContent,error){
+	log.Printf("GetCtx:%#v",shmparam)
 	sm,err:= CreateWithKey(shmparam.Key,0)
 	if err != nil {
 		log.Fatal(err)
-		return updatectx,err
+		return nil,err
 	}
 	log.Print(sm)
-	pos ,err:=sm.ReadObjCtx(updatectx)
-	if err != nil {
+	data:=make([]byte,sizeOfTagTLVStruct)
+	pos ,err:=sm.Read(data)
+	if err !=nil {
 		log.Fatal(err)
+		return nil,err
 	}
-	log.Print(pos)
-	return updatectx,err
+	log.Println(pos)
+	tlv:=BytesToTagTLVStruct(data)
+	ctd:=new(UpdateContent)
+	ctd.Topic=string(tlv.Topic[:])
+	ctd.Content=string(tlv.Value[:])
+
+	return ctd,nil
 }
